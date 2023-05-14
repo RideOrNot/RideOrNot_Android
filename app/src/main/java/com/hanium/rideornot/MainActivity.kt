7c2fb26303f1261987e14761d0b16c1e66d52625
최지은 <cje172@naver.com>
2023-05-14 13:29:08 +0900

    Feat: #1 Bottom Navigation 구현

--- app/src/main/java/com/hanium/rideornot/MainActivity.kt
+++ app/src/main/java/com/hanium/rideornot/MainActivity.kt
@@ -2,10 +2,52 @@ package com.hanium.rideornot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hanium.rideornot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frm_main, HomeFragment())
+            .commitAllowingStateLoss()
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frm_main, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frm_main, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.favoriteFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frm_main, FavoriteFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.settingFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frm_main, SettingFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
