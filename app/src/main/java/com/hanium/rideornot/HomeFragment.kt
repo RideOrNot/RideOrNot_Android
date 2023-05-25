bc8b6bdec0449d8c5f07a80133ecf2197932bae8
dioo1461 <dioo1461@naver.com>
2023-05-25 20:43:10 +0900

    Feat: #2 gps 현위치 update 기능 구현

--- app/src/main/java/com/hanium/rideornot/HomeFragment.kt
+++ app/src/main/java/com/hanium/rideornot/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.hanium.rideornot
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.hanium.rideornot.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,8 +19,28 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mainActivity = activity as MainActivity
+
+        val locationTestButton: Button? = view?.findViewById(R.id.locationTestButton)
+        locationTestButton?.setOnClickListener {
+            Log.d("test","location: ${mainActivity.lastLocation}")
+        }
+
+        val startLocationUpdateButton: Button? = view?.findViewById(R.id.startLocationUpdateButton)
+        startLocationUpdateButton?.setOnClickListener {
+            mainActivity.startLocationUpdates()
+        }
+
+        val stopLocationUpdateButton: Button? = view?.findViewById(R.id.stopLocationUpdateButton)
+        stopLocationUpdateButton?.setOnClickListener {
+            mainActivity.stopLocationUpdates()
+        }
+
     }
 
 }
--- app/src/main/java/com/hanium/rideornot/MainActivity.kt
+++ app/src/main/java/com/hanium/rideornot/MainActivity.kt
@@ -1,25 +1,58 @@
 package com.hanium.rideornot
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import com.google.android.gms.tasks.Task
 import com.hanium.rideornot.databinding.ActivityMainBinding
+
+private const val LOCATION_REQUEST_PERMISSION_REQUEST_CODE: Int = 1
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    // private lateinit var locationManager: LocationManager
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    public var lastLocation: Location? = null
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var cancellationTokenSource: CancellationTokenSource
+
+
+    // private val REQUEST_PERMISSION_LOCATION = 10
+
+    private lateinit var geofencingClient: GeofencingClient
+    private val geofenceList = MutableList<Geofence?>(100) { null }
+
+   private var locationUpdateInterval: Long = 2000;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
-
         initBottomNavigation()
+
+        initLocationService()
+        startLocationUpdates()
+
+        //geofencingClient = LocationServices.getGeofencingClient(this)
     }
 
     private fun initBottomNavigation() {
@@ -57,8 +90,67 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+//    private fun getStationLocation(longitude:Float, latitude:Float): Task<Location>? {
+//       return null
+//    }
 
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates() {
+        locationRequest = LocationRequest.create().apply{
+            interval = locationUpdateInterval
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+        }
 
+        var tempIndex = 0;
+        locationCallback = object: LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                lastLocation = p0.lastLocation
+
+                tempIndex++
+                Log.d("locationUpdate-attempt $tempIndex",
+                    "longitude: ${lastLocation?.longitude.toString()}, latitude: ${lastLocation?.latitude.toString()}")
+            }
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            // 위치 업데이트를 처리할 스레드의 루퍼를 전달
+            Looper.getMainLooper()
+        )
+    }
+
+    fun stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun initLocationService() {
+        cancellationTokenSource = CancellationTokenSource()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        val cancellationToken: CancellationToken = cancellationTokenSource.token
+        // FINE_LOCATION, COARSE_LOCATION 의 permission check
+        // TODO: 두 권한 체크를 분리해야 할지?
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                // TODO: request codes 관리 방법 모색
+                LOCATION_REQUEST_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
 
 
 
